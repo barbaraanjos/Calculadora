e74fbecd574f59afd3c9e199bed26017a93067f3
barbara.anjos <barbara.anjos@segfy.com>
2024-04-16 19:55:57 -0300

    Add project files.

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,58 @@
+﻿float primeiroNumero;
+float segundoNumero;
+float adicao;
+float subtracao;
+float multiplicacao;
+float divisao;
+
+//Coleta valores do console
+void ColetarValores()
+{
+	Console.Write("Digite o primeiro número: ");
+	primeiroNumero = float.Parse(Console.ReadLine()!);
+	Console.Write("Digite o segundo número: ");
+	segundoNumero = float.Parse(Console.ReadLine()!);
+
+	Adicao();
+	Subtracao();
+	Multiplicacao();
+	Divisao();
+	
+	ExibeResultados();
+}
+
+//Operações
+float Adicao()
+{
+	adicao = primeiroNumero + segundoNumero;
+	return adicao;
+}
+
+float Subtracao()
+{
+	subtracao = primeiroNumero - segundoNumero;
+	return subtracao;
+}
+
+float Multiplicacao()
+{
+	multiplicacao = primeiroNumero * segundoNumero;
+	return multiplicacao;
+}
+
+float Divisao()
+{
+	divisao = primeiroNumero / segundoNumero;
+	return divisao;
+}
+
+//Exibe resultados
+void ExibeResultados()
+{
+	Console.WriteLine($"A soma {primeiroNumero} + {segundoNumero} é: {adicao}");
+	Console.WriteLine($"A subtração {primeiroNumero} - {segundoNumero} é: {subtracao}");
+	Console.WriteLine($"A multiplicação {primeiroNumero} * {segundoNumero} é: {multiplicacao}");
+	Console.WriteLine($"A divisao {primeiroNumero} / {segundoNumero} é: {divisao}");
+}
+
+ColetarValores();
---
+++ CalculadoraDeLista/CalculadoraDeLista.cs
@@ -0,0 +1,38 @@
+﻿List<int> listaDeNumeros = new List<int>();
+int tamanhoDaLista;
+int adicao = 0;
+
+//Coletando os valores
+void ColetarValores()
+{
+	Console.Write("Digite a quantidade de números que sua lista terá: ");
+	tamanhoDaLista = int.Parse(Console.ReadLine()!);
+	Console.Write("Digite os números da sua lista: ");
+
+	for (int i = 0; i < tamanhoDaLista; i++)
+	{
+		int numeroDaLista = int.Parse(Console.ReadLine()!);
+		listaDeNumeros.Add(numeroDaLista);
+	}
+
+	Adicao();
+
+	ExibirResultados();
+}
+
+//Operações
+int Adicao()
+{
+	foreach (int numero in listaDeNumeros)
+	{
+		adicao += numero;
+	}
+	return adicao;
+}
+
+void ExibirResultados()
+{
+	Console.WriteLine($"A soma dos números da lista é: {adicao}");
+}
+
+ColetarValores();
